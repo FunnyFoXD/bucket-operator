a2ef19a271a00db192e1ed2b51ada1de6037bf00
Daniil <tskhe_daniil@mail.ru>
2025-12-26 21:14:37 +0300

    feat: add finalizer

--- internal/controller/bucket_controller.go
+++ internal/controller/bucket_controller.go
@@ -19,9 +19,11 @@ package controller
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	storagev1alpha1 "github.com/funnyfoxd/bucket-operator/api/v1alpha1"
@@ -47,19 +49,60 @@ type BucketReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.4/pkg/reconcile
 func (r *BucketReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	// Initilize logger
 	log := logf.FromContext(ctx).WithValues("bucket", req.NamespacedName)
 
+	// Read and fetch "bucket" resource
 	var bucket storagev1alpha1.Bucket
 	if err := r.Get(ctx, req.NamespacedName, &bucket); err != nil {
-		log.Error(err, "unable to fetch Bucket")
-		return ctrl.Result{}, err
+		if !errors.IsNotFound(err) {
+			log.Error(err, "unable to fetch Bucket")
+		}
+
+		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
 	log.Info("Reconcile is triggered", "size", bucket.Spec.Size)
 
+	// Finalizer init and logic
+	finalizer := "bucket-controller.storage.mydomain.com/finalizer"
+
+	if !bucket.ObjectMeta.DeletionTimestamp.IsZero() {
+		log.Info("Bucket deletion in progress...")
+		if controllerutil.ContainsFinalizer(&bucket, finalizer) {
+			log.Info("Removing finalizer...")
+			controllerutil.RemoveFinalizer(&bucket, finalizer)
+
+			if err := r.Update(ctx, &bucket); err != nil {
+				log.Error(err, "couldn't remove finalizer")
+
+				return ctrl.Result{}, err
+			}
+			log.Info("Finilizer is removed")
+		}
+	} else {
+		if !controllerutil.ContainsFinalizer(&bucket, finalizer) {
+			log.Info("Adding finalizer...")
+			controllerutil.AddFinalizer(&bucket, finalizer)
+
+			if err := r.Update(ctx, &bucket); err != nil {
+				log.Error(err, "couldn't add finalizer")
+
+				return ctrl.Result{}, err
+			}
+			log.Info("Finalizer is added")
+		}
+	}
+
+	// Change status
 	if bucket.Status.Phase != "Ready" {
 		bucket.Status.Phase = "Ready"
 		if err := r.Status().Update(ctx, &bucket); err != nil {
+			if errors.IsConflict(err) {
+				log.Info("Status conflict - retrying", "error", err)
+
+				return ctrl.Result{Requeue: true}, err
+			}
 			log.Error(err, "unable to update status")
 			return ctrl.Result{}, err
 		}
