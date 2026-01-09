b06011551116e2b9c6dc608950c9b999273718c7
Daniil <tskhe_daniil@mail.ru>
2026-01-09 19:26:33 +0300

    feat: add webhook files and create validation webhook

--- cmd/main.go
+++ cmd/main.go
@@ -185,6 +185,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Bucket")
 		os.Exit(1)
 	}
+	// nolint:goconst
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err := (&storagev1alpha1.Bucket{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Bucket")
+			os.Exit(1)
+		}
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
